c93e7ac16a805feffc3bbea87f1c9063c994c6a4
sefi2 <sefi2@unist.ac.kr>
2019-05-17 12:28:21 +0900

    testing

--- test/GroupEvalTest.js
+++ test/GroupEvalTest.js
@@ -1,9 +1,18 @@
 
 
-const GroupEval = artifacts.require('GroupEval');
+const GroupEvalContract = artifacts.require('./GroupEval.sol');
+const UCoinContract = artifacts.require('./UCoin.sol');
 
 contract('GroupEval', (accounts) => {
+		it("should init UCoin address", async () => {
+			const UCoin = UCoinContract.deployed()
+			const GroupEval = GroupEvalContract.deployed()
+			console.log(GroupEval.ucoin)
 
+			await GroupEval.initUCoinAddress(UCoin.address);
+			console.log(GroupEval.ucoin)
+				
+		});
 
 
 });
